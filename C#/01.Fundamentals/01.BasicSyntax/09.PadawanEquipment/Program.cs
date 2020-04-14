c986e7b8a932e82ee6b732dffd0af22877f585f8
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:50:20 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/01.BasicSyntax/09.PadawanEquipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double lightsabrePrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double lightsabreMoney = (Math.Ceiling(1.1 * students)) * lightsabrePrice;
+            double robeMoney = students * robePrice;
+            double beltMoney = (students - (students / 6)) * beltPrice;
+
+            double moneyNeeded = lightsabreMoney + robeMoney + beltMoney;
+
+            if (moneyNeeded <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {moneyNeeded:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(moneyNeeded - money):f2}lv more.");
+            }
+        }
+    }
+}
