01547a2fb4c93c209e69d4f33f243fd0e591880c
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:47:44 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/01.BasicSyntax/07.VendingMachine/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace _07.VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double money = 0;
+
+            while (input != "Start")
+            {
+                double coin = double.Parse(input);
+
+                if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1.0 || coin == 2.0)
+                {
+                    money += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string product = Console.ReadLine();
+
+            while (product != "End")
+            {
+                if (product == "Nuts")
+                {
+                    if (2 <= money)
+                    {
+                        Console.WriteLine("Purchased nuts");
+                        money -= 2.0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (product == "Water")
+                {
+                    if (0.7 <= money)
+                    {
+                        Console.WriteLine("Purchased water");
+                        money -= 0.7;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (product == "Crisps")
+                {
+                    if (1.5 <= money)
+                    {
+                        Console.WriteLine("Purchased crisps");
+                        money -= 1.5;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (product == "Soda")
+                {
+                    if (0.8 <= money)
+                    {
+                        Console.WriteLine("Purchased soda");
+                        money -= 0.8;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else if (product == "Coke")
+                {
+                    if (1.0 <= money)
+                    {
+                        Console.WriteLine("Purchased coke");
+                        money -= 1.0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, not enough money");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+                }
+
+                product = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Change: {money:f2}");
+        }
+    }
+}
