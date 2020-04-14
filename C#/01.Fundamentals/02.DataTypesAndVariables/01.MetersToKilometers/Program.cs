7484c60e4c66273bc269dd2cd98054590e276117
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:54:04 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/02.DataTypesAndVariables/01.MetersToKilometers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.MetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            double kilometers = meters / 1000.00;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
