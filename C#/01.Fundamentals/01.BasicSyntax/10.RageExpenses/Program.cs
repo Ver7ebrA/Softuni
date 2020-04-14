7d8752cb82d77ebe5d93ba320df2a248e1d45c85
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:51:22 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/01.BasicSyntax/10.RageExpenses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double expenses = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    expenses += headsetPrice;
+                }
+
+                if (i % 3 == 0)
+                {
+                    expenses += mousePrice;
+                }
+
+                if (i % 6 == 0)
+                {
+                    expenses += keyboardPrice;
+                }
+
+                if (i % 12 == 0)
+                {
+                    expenses += displayPrice;
+                }
+            }
+
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
