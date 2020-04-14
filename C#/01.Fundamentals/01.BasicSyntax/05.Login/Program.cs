1f5e5424ee4e2f9fead4437ca98ea988c9993914
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:45:36 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/01.BasicSyntax/05.Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            string correctPassword = "";
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                correctPassword += username[i];
+            }
+
+            string password = Console.ReadLine();
+            int counter = 0;
+
+            while (password != correctPassword)
+            {
+                if (counter == 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+                password = Console.ReadLine();
+                counter++;
+
+
+            }
+
+            if (password == correctPassword)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
