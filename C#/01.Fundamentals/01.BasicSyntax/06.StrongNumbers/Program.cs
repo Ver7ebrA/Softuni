637f2170712eb177f77492e30393cbd8b763fc59
Ver7ebrA <63633996+Ver7ebrA@users.noreply.github.com>
2020-04-14 11:46:21 +0300

    Add files via upload

---
+++ C#/01.Fundamentals/01.BasicSyntax/06.StrongNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06.StrongNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string input = number.ToString();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int num = (int)Char.GetNumericValue(input[i]);
+            }
+        }
+    }
+}
